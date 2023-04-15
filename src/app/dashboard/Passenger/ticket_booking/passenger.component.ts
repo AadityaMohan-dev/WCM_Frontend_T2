4cba6590d477096e9050d6eb1a4538399aad8815
Aaditya Mohan <aadimohan51@gmail.com>
2023-04-16 00:49:55 +0530

    passenge to paymnt rounting added

--- src/app/dashboard/Passenger/ticket_booking/passenger.component.ts
+++ src/app/dashboard/Passenger/ticket_booking/passenger.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-passenger',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./passenger.component.css']
 })
 export class PassengerComponent {
+
 
 }
